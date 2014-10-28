208f003d364d38d49eaf1ee5725ff049d4576734
zrong <zrongzrong@gmail.com>
2014-10-28 22:43:03 +0800

    update category

--- buildreadme.py
+++ buildreadme.py
@@ -3,6 +3,17 @@
 import os
 import re
 
+def _read_file(afile):
+    with open(afile, 'r', encoding='utf-8') as f:
+        content = f.read()
+    return content
+
+def _get_mdfile(adir):
+    for afile in os.listdir(adir):
+        if afile.endswith('.md'):
+            name = afile.split('.')[0]
+            yield (adir, name, os.path.join(adir, afile))
+
 def _write_list(adir, rf):
     rf.write('# '+adir+'\n\n')
     is_post = adir == 'post'
@@ -81,8 +92,29 @@ def _rewrite_url(adir):
                     f.write(content)
                     print(fpath)
 
+def _rewrite_category():
+    sign = 'Category: '
+    num = 0
+    for adir,name,fpath in _get_mdfile('post'):
+        content = None
+        with open(fpath, 'r', encoding='utf-8', newline='\n') as f:
+            for line in f:
+                if line.startswith(sign):
+                    f.seek(0)
+                    line = line[len(sign):]
+                    cats = line.split(',')
+                    if len(cats) > 1:
+                        content = True
+                    break
+        if content:
+            print(name, cats)
+            num = num + 1
+            # with open(fpath, 'w', encoding='utf-8', newline='\n') as f:
+            #     f.write(content)
+    print(num)
 
 if __name__ == "__main__":
     #_write_readme()
     #_rewrite_title()
-    _rewrite_url('post')
+    #_rewrite_url('post')
+    _rewrite_category()
